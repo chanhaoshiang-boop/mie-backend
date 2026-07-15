8d47f093952560729190d0ab79436cdb885a2c00
chanhaoshiang-boop <chanhaoshiang@gmail.com>
2026-07-16 07:37:59 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => res.send('OK'));
+
 // ==========================================
 // 資料庫
 // ==========================================
